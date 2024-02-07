61cdfd1f69036a7e0723083e0e70f18efae8c06a
Cewkins <128816143+Cewkins@users.noreply.github.com>
2024-02-07 22:24:29 +0400

    Update example.py

--- example.py
+++ example.py
@@ -1,8 +1,4 @@
 def git_opeation():
  print("I am adding example.py file to the remote repository.")
 git_opeation()
-#this is a test
-#edit 2
-#edit 3, this time with main branch
-#edit 4
-#edit 5
+#removed useless lines of code
